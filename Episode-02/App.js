dcb32d0a06d3c4edb9dad20ee5a3b90ac5fd0f8c
Ankit-Dhingra <ankitdhingra1909@gmail.com>
2024-12-14 16:09:56 +0530

    "Episode-02"

---
+++ Episode-02/App.js
@@ -0,0 +1,34 @@
+// Complete this Task Using React
+{
+  /* <div id="parent">
+    <div id="child1">
+        <h1>Hello I'am H1 Tag</h1>
+        <h2>Hello I'am H2 Tag</h2>
+    </div>
+    <div id="child2">
+        <h1>Hello I'am H1 Tag</h1>
+        <h2>Hello I'am H2 Tag</h2>
+    </div>
+</div> */
+}
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+ [
+    React.createElement("div", { id: "child1" }, [
+        React.createElement("h1", {}, "Hello I'm H1 Tag"),
+        React.createElement("h2", {}, "Hello I'm H2 Tag"),
+      ]),
+      React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "Hello I'm H1 Tag"),
+        React.createElement("h2", {}, "Hello I'm H2 Tag"),
+      ])
+ ]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
